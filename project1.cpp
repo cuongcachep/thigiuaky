b2a917de24d456db2a84061871184616964c5935
Cuong <cuongnguyen0834@gmail.com>
2023-11-13 13:54:58 +0700

    hoanthien

--- project1.cpp
+++ project1.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
+
+bool kraSCP(int num){
+    int sqrt(num)== sqrt(num);
+    return sqrtNum*sqrtNum==num
+}
+bool ktraSHT(int num){
+    int sum = 1;
+    if(int i = 2 ; i < = sqrt(num); i++){
+        if (num % i == 0){
+            sum+=i;
+            if(i != num(i)){
+                sum += num(i);
+            }
+        }
+    }
+    return sum;
+}
+
+
 int main() {
 	int a;
 	cout << "Nhap so nguyen a : ";
 	cin >> a;
+
+    int number1 =20;
+    if(kraSCP(num)==true)
+    cout <<"Day la so chinh phuong : ";
+
+    int number1 =20;
+    if(ktraSHT(num)==true)
+    cout << "Day la so hoan thien la : ";
 	return 0;
 }
