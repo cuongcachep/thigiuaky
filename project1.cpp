c8b8856dbc3944df9d7eebea8327f3f1f48cfd7e
Cuong <cuongnguyen0834@gmail.com>
2023-11-13 13:40:57 +0700

    sochinhphuong

--- project1.cpp
+++ project1.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
+
+bool kraSCP(int num){
+    int sqrt(num)== sqrt(num);
+    return sqrtNum*sqrtNum==num
+}
+
+
+
 int main() {
 	int a;
 	cout << "Nhap so nguyen a : ";
 	cin >> a;
+
+    int number1 =20
+    if(kraSCP(num)==true)
+    cout <<"Day la so chinh phuong : ";
 	return 0;
 }
